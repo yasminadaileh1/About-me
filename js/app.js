8566afbcb40dfe6d251dba9e8be94961a61bc96d
Reham-omar <rehamalsobh96@gmail.com>
2020-02-19 15:16:07 +0200

    adding function

--- js/app.js
+++ js/app.js
@@ -1,62 +1,69 @@
 'use strict';
 var score = 0; 
+function userName(){
 var name = prompt("Hello whats your name?");
 alert(" welcome " + name + " we are happy to have you here!");
+} userName();
 console.log(name);
 
+function myage(age){
 var age = prompt('How old I am ?');Number;
 if (age == 23 ) {
     alert("Yesss! thats right..");
     score ++ ;
 } else if (age !== 23) {
     alert('No wrong answer. I\'m 23');
-}
+}}
+myage();
+
 //console.log(age);
 
-
+function fromWhere (){
 var fromWhere = prompt('I\'m from jordan yes or no ?').toLocaleLowerCase();
 if (fromWhere == 'yes' || fromWhere == 'y' ) {
     alert('correct answer');
     score++ ;
 } else if (fromWhere == 'no' || fromWhere == 'n'  )  {
     alert('I\'m not from ' + fromWhere + '. I\'m from Karak');
-}
+}}
+fromWhere();
 //console.log(fromWhere);
 
+function mymajor(){
 var major = prompt('my major is accounting right?').toLocaleLowerCase();
 if (major == 'yes' || major == 'y' ) {
     alert('you are doing great! ');
     score++ ;
 } else if (major == 'no' || major == 'n' ) {
     alert(  'my major is Accounting');
-}
+}}
+mymajor();
 //console.log(major);
 
+function myfeedback(){
 var feedBack = prompt(' do you think that I\'m a good in coding or not').toLocaleLowerCase();
 if (feedBack == 'yes' || feedBack == 'y' ) {
     alert('Thank you!');
     score ++ ; 
 } else if (feedBack == 'no'|| feedBack == 'n') {
     alert(' I\'m good in coding !!');
-}
+}}
+myfeedback();
 //console.log(feedBack);
 
+function myhope(){
 var hope = prompt('Did you think I have any hope?').toLocaleLowerCase();
 if (hope == 'yes' || hope == 'y') {
     alert('great I have a hope' );
     score ++ ;
 } else if (hope == 'no' || hope == 'n') {
     alert('I have a hope!');
-}
+}}
+myhope();
+
 //console.log(hope);
 
-
-
-
-alert(" wish you have a good day " + name);
-
-
-
+function myFavNum(){
     var favNum = prompt('what\'s my fav number? (from 1 to 10) you have four atempt');
     favNum = Number(favNum);
     for ( var i = 0 ; i < 3; i++){
@@ -76,26 +83,27 @@ alert(" wish you have a good day " + name);
         alert('sorry! your attempts finished');
         break;
     }
-    }
+    }}
+    myFavNum();
     
-        
+     function myFavFood(){  
     var favFood =['mansaf', 'burger', 'kabseh', 'mskhan'];
-    var myFav = prompt('what\'s my favorite food');
     
     for ( var f = 0 ; f < 6 ; f++){
-        if (myFav === favFood[0] || myFav ===favFood[1] || myFav === favFood[2]|| myFav === favFood[3]){
+        var myFav = prompt('what\'s my favorite food').toLocaleLowerCase();
+        
+        for (var k=0 ; k<favFood.length;k++){
+           if (myFav===favFood[k]){
             alert ('Wow you guess it');
             score++;
-            break;
-        }else {
+           k=9;
+        f=9;
+        } else{ 
             alert( 'wrong! try again');
-        } 
-         prompt('what\'s my favorite food');
-        if (f === 5){
-        alert('sorry! your attempts finished');
-        break;
-        }
-        }
+
+        }}
+        }}
+        myFavFood();
       
     
         
